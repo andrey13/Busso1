7e569e2902d71fac0d4504466f1c415742a84f38
Andrey Krashenitza <andrey13@yandex.ru>
2022-08-29 16:16:14 +0300

    init

---
+++ app/src/main/java/com/raywenderlich/android/busso/ui/view/busarrival/BusArrivalItemViewHolder.kt
@@ -0,0 +1,36 @@
+package com.raywenderlich.android.busso.ui.view.busarrival
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.raywenderlich.android.busso.R
+
+/**
+ * The ViewHolder for the List of Arrivals for a BusStop
+ */
+
+class BusArrivalItemViewHolder(
+    itemView: View
+) : RecyclerView.ViewHolder(itemView) {
+
+  private val busArrivalLineName: TextView =
+      itemView.findViewById(R.id.bus_arrival_line_and_destination)
+  private val arrivalTimesAdapter = BusArrivalTimeAdapter()
+  private val timesRecyclerView: RecyclerView =
+      itemView.findViewById<RecyclerView>(R.id.bus_arrival_times_recyclerview).apply {
+        val viewManager =
+            LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+        layoutManager = viewManager
+        adapter = arrivalTimesAdapter
+      }
+
+
+  lateinit var busStopListModel: BusArrivalGroupViewModel
+
+  fun bind(itemViewModel: BusArrivalGroupViewModel) {
+    busStopListModel = itemViewModel
+    busArrivalLineName.text = "${busStopListModel.lineName} to ${busStopListModel.destination}"
+    arrivalTimesAdapter.submitList(itemViewModel.arrivals)
+  }
+}
